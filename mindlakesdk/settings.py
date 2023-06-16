5d151df00496413389657b290bb0ec2b181528a8
Dennis <dennis@mindnetwork.xyz>
2023-06-16 19:02:58 +0800

    update version number

--- mindlakesdk/settings.py
+++ mindlakesdk/settings.py
@@ -1,4 +1,4 @@
-GATEWAY = 'http://sdk.mindnetwork.xyz/node'
+GATEWAY = 'https://sdk.mindnetwork.xyz/node'
 WEB3API = 'https://goerli.infura.io/v3/744c0ade89464e4b867ca1b002a10231'
 CONTRACT_ADDRESS = '0xF5932e67e84F08965DC6D62C2B67f47a6826E5a7'
 CONTRACT_ABI = [
@@ -70,4 +70,4 @@ CONTRACT_ABI = [
                 "type": "function"
         }
 ]
-VERSION = 'v1.0.0'
+VERSION = 'v1.0.2'
