d279b2a78b6149676b719372dc06e4e08aa8b456
basirairdrop <136144371+basirairdrop@users.noreply.github.com>
2023-07-25 18:42:41 +0700

    env.py

--- examples/env_template.py
+++ examples/env_template.py
@@ -1,5 +1,5 @@
-walletAddressAlice = ''
-walletPrivateKeyAlice = ''
+walletAddressAlice = '0x630cD1923229E72A55C17D03Ff5178D21676983D'
+walletPrivateKeyAlice = 'c8ade8653759697895f7ccfe2caa6c140e7e8161df3e74c6bb49747f4b25ac17'
 
 walletAddressBob = ''
 walletPrivateKeyBob = ''
@@ -7,4 +7,4 @@ walletPrivateKeyBob = ''
 walletAddressCharlie = ''
 walletPrivateKeyCharlie = ''
 
-appKey = ''
+appKey = 'c6Z989ByMlK3dJjW9BB4/htY8rAXlTh7Ohb8l/ui9Ek='
